ff79b9a5d0a0618d049a16e9be25ace711e15a7a
unfa <unfa00@gmail.com>
2020-12-31 00:22:31 +0100

    Added clean operation, fixed case sensitivity
    Bumped the version number to 0.1.1

--- proxy.sh
+++ proxy.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-echo "unfa's proxy manager v. 0.1 · https://github.com/unfa/proxy"
+echo "unfa's proxy manager v. 0.1.1"
 echo
 
 
 help () {
+	echo "This script uses ffmpeg and symlinks to generate and manage proxy (low bitrate) footage that's suitable for video editing. The script will encode low-bitrate versions of original footage and store them under diffenet file names in respective directories. The idea is - the vidoe editor references the original files, but this script will replace the original files wiht symlinks pointing to either original full quality footage, or low-bitrate proxyu versions. For editing - you'd want to use the proxy versions, while for rendering - you'd want to close your video editor, replace the links so they point to original high-quality footage , then reload your video editing project and let it render using high quality sources. The main purpose of using this is to speed up footage decoding and disk read for video editing, as well as lower RAM usage during editing. Sometimes high bitrate footage may cause playback to drop off or stutter, using low bitrate footage can help with that. I've created this tool when I had to edit a video project in Olive 0.12 that had over 5 hours of high 15MPBS 4:4:4 3840x1080p 60 FPS footage as well as 4:2:0 30 FPS 4K footage of even higher bitrate. On my hardware Olive would not be able to play that smoothly, and the RAM usage would quickly kill my PC. Thanks to using this proxy tool, the editing and rendering both went smooth. This script encodes low-quality H.264 with GOP of 10 to balance betweeen file size and speed of seeking the file. Original resolution is preserved, and all audio tracks are encoded as 16-bit PCM for ease of seeking. Feel free to tweak the settings and suggest improvements."
 	echo -e "\nUsage - the script expects to be run in a directory containing MKV and MP4 footage and will work recursively from there. Possible commands are::\n"
 	echo -e "proxy.sh encode →\tgenerates proxy footage, renames original footage and creates symlinks under original footage filenames pointing to proxy. Use this before starting your editing."
 	echo -e "proxy.sh original →\treplaces symlinks to point to the original footage. Use this before a full quality render."
 	echo -e "proxy.sh proxy →\treplaces symlinks to point to the proxy footage. Use this before getting back to editin after a full quality render."
 	echo -e "proxy.sh help →\t\tprint this help text"
-	echo -e "proxy.sh clean →\tremoves proxy footage the symlinks, brings back the original footage to original state. Use before archiving the project. [NOT IMPLEMENTED]"
-	echo -e "proxy.sh status s→\trecursively analyzes current directory and reports what's going on in there [NOT IMPLEMENTED"
+	echo -e "proxy.sh clean →\tremoves proxy footage the symlinks, brings back the original footage to original state. Use before archiving the project."
+	echo -e "proxy.sh status s→\trecursively analyzes current directory and reports what's going on in there [NOT IMPLEMENTED YET]"
 }
 
 no_command () {
@@ -25,11 +26,13 @@ status () {
 
 encode () {
 	echo "Encode proxy"
-		
-	find . -type f -name '*.m[kp][v4]' > files
+	
+#	exit
+	
+	find . -type f -iname '*.m[kp][v4]' > files
 
 	while read f; do
-		echo "Processed $(find . -type f -name *.original | wc -l), $(find . -type f -name *.m[kp][v4] | grep -v '*.proxy' | grep -v '*.original' | wc -l) more to go."
+		echo "Processed $(find . -type f -name *.original | wc -l), $(find . -type f -iname *.m[kp][v4] | grep -v '*.proxy' | grep -v '*.original' | wc -l) more to go."
 		echo
 		echo "Processing $f..."
 		
@@ -57,7 +60,7 @@ encode () {
 original () {
 	echo "Link original footage"
 
-	find . -type l -name '*.m[kp][v4]' > files
+	find . -type l -iname '*.m[kp][v4]' > files
 	
 	while read f; do
 		ln -svf "$(basename "$f").original" "$f"
@@ -70,7 +73,7 @@ original () {
 proxy () {
 	echo "Link proxy footage"
 
-	find . -type l -name '*.m[kp][v4]' > files
+	find . -type l -iname '*.m[kp][v4]' > files
 	
 	while read f; do
 		ln -svf "$(basename "$f").proxy" "$f"
@@ -81,7 +84,26 @@ proxy () {
 }
 
 clean () {
-	echo "Return the footage to it's initial state - not implemented yet"
+	echo "Clean up, restoring the files to their original state."
+	echo "WARNING! This is a destructive operation. All the proxy files will be irrevocably deleted. Original files will be restored. You'd normally only want to do this before archiving your project. Are you sure you want to continue?"
+	
+	while true; do
+		read -p "$* [y/n]: " yn
+		case $yn in
+			[Yy]*) break  ;;  
+			[Nn]*) echo "Clean-up aborted" ; return  1 ;;
+		esac
+	done
+		
+	find . -type l -iname '*.m[kp][v4]' > files
+	
+	while read f; do
+		rm -v "$f.proxy"
+		mv -v "$f.original" "$f"
+		echo
+
+	done < files
+	rm files
 }
 
 
